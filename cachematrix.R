027e87ce5ea463bb9090523779e0960f9e99ffcf
duvvurum <raja.duvvuru@gmail.com>
2014-04-24 14:27:56 +0530

    Comments added to the code

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,22 @@
 # Below "makeCacheMatrix" function below will creat a special 
 ##matrix object that will catch the inverse of the matrix
 #===============================================================
-
+# Begining of "makeCacheMatrix" function
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    m <- NULL # initiating empty inverse matrix
     set <- function(y){
-        x <<- y
+        x <<- y  # assigning external matrix
         m <<- NULL
     }
     get <- function() x
-    setsolve <- function(solve) m<<- solve
-    getsolve <- function()m
+    setsolve <- function(solve) m<<- solve # creating the inverse matrix
+    getsolve <- function()m # obtaining the value of m from the environment out side the function
     list(set = set,
          get = get,
          setsolve = setsolve,
          getsolve = getsolve)
 }
-
+# end of function "makeCacheMatrix"
 #===========================================================
 # "cacheSolve" function below will compute the inverse of 
 ##the special matrix returned by "makeCacheMatrix".
@@ -27,15 +27,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ####and the matrix has not changed, it will retrive the 
 #####inverse from the cache.
 #===========================================================
-
+# begining of "cacheSolve" function
 cacheSolve <- function(x, ...) {
-    m <- x$getsolve()
-    if(!is.null(m) & identical(x)){
-        message("getting cached inverse")
-        return(m)
+    m <- x$getsolve()  # getting the value of 'm'  
+    if(!is.null(m) & identical(x)){ # checking that m is not null and matrix is not changed
+        message("getting cached inverse") 
+        return(m) # printing the inverse of the matrix
     }
-    data <- x$get()
-    m <- solve(data, ...)
+    data <- x$get() # read input matrix  to data
+    m <- solve(data, ...) # create the inverse of matrix and store in m
     x$setsolve(m)
-    m
+    m # return inverse matrix stored in 'm'
 }
+#end of "cacheSolve" function.
